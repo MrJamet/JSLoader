5229a7e28bacc2a357aeaa7cb58ab56ff142ca3a
MrJamet <gregory.jamet@live.fr>
2024-01-31 21:50:01 +0100

    Ajout des cercles

--- index.js
+++ index.js
@@ -8,14 +8,39 @@ class SpinningDots extends HTMLElement{
         const root = this.attachShadow({mode: 'open'})
         root.innerHTML = `<div>
             ${this.buildStyle(width)}
+            ${this.buildCircles(width, circles, circleRadius)}
         </div>`
     }
     /**
-     * 
-     * @param {number} w largeur de la div 
+     * Construit un SVG contenant nos cercles
+     * @param {number} w Largeur du SVG
+     * @param {number} n Nombre de cercles
+     * @param {number} r Rayon de chaque cercle
+     * @return {string}
+     */
+    buildCircles(w, n, r){
+        let dom = `<svg width="${w}" height="${w}" viewBox="0 0 ${w} ${w}">`
+        dom += `<circle cx="${w/2}" cy="${w/2}" r="${r}"/>`
+
+        for(let i=0; i<n; i++){
+            //angle de 360°c=pi*2, divisé par 8 cercles, multiplié par chacun
+            //0, 45, 90, 135, 180, ...
+            const angle = i * (Math.PI * 2) / n
+
+        }
+
+        return dom + `</svg>`
+    }
+    /**
+     * Construire le style de notre loader
+     * @param {number} w largeur de l'élément 
+     * @return {string}
      */
     buildStyle(w){
         return `<style>
+                :host{
+                    display: inline-block;
+                }
                 div{
                     width: ${w}px;
                     height: ${w}px;
@@ -23,37 +48,16 @@ class SpinningDots extends HTMLElement{
                 }
             </style>`
     }
-    
-    /**${this.buildCircle(w, circles, circleRadius)} */
-    
-    /**
-     * Construit le style de notre loader
-     * @param {number} w Largeur de l'élément
-     * @returns {string}
-     */
-    // buildStyle(w){
-    //     return `<style>
-    //         div{
-    //             height: 40px;
-    //             width: 40px;
-    //             background: red;
-    //         }
-    //     </style>`
-    // }
 }
 
-try
-{
+try{
     customElements.define('spinning-dots', SpinningDots)
 }
-catch(e)
-{
-    if (e instanceof DOMException)
-    {
+catch(e){
+    if (e instanceof DOMException)    {
         console.error('DOMException : ' + e.message);
     }
-    else
-    {
+    else{
         throw e
     }
 }
