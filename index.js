9f61258a67bf6f584acfdb6e3242c0fd358d629f
MrJamet <gregory.jamet@live.fr>
2024-02-13 21:53:53 +0100

    finish

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ class SpinningDots extends HTMLElement{
         const circleRadius = 2
         const root = this.attachShadow({mode: 'open'})
         root.innerHTML = `<div>
-            ${this.buildStyle(width)}
+            ${this.buildStyle(width, circleRadius * 2, circles)}
             ${this.buildCircles(width, circles, circleRadius)}
+            ${this.buildTrail(width/2 - circleRadius, circleRadius * 2)}
         </div>`
     }
     /**
@@ -19,24 +20,52 @@ class SpinningDots extends HTMLElement{
      * @return {string}
      */
     buildCircles(w, n, r){
-        let dom = `<svg width="${w}" height="${w}" viewBox="0 0 ${w} ${w}">`
-        dom += `<circle cx="${w/2}" cy="${w/2}" r="${r}"/>`
-
+        //class circles pour animé le cercle tournant
+        let dom = `<svg class="circles" width="${w}" height="${w}" viewBox="0 0 ${w} ${w}">`
+        const radius = ((w / 2) - r)
         for(let i=0; i<n; i++){
             //angle de 360°c=pi*2, divisé par 8 cercles, multiplié par chacun
             //0, 45, 90, 135, 180, ...
             const angle = i * (Math.PI * 2) / n
-
+            //position en x
+            const posX = (radius * Math.sin(angle)) + (w / 2)
+            //position en y
+            const posY = (radius * Math.cos(angle)) + (w / 2)
+            //ajout d'un cercle
+            dom += `<circle cx="${posX}" cy="${posY}" r="${r}"/>`
         }
 
         return dom + `</svg>`
     }
     /**
+     * 
+     * @param {number} r Rayon du cercle
+     * @param {number} stroke Epaisseur du trait
+     */
+    buildTrail (r, stroke){
+        const w = (r * 2) + stroke
+        let dom = `<svg class="trail" width="${w}" height="${w}"
+        viewBox="0 0 ${w} ${w}" fill="none">`
+        dom += `<circle 
+        cx="${w / 2}" 
+        cy="${w / 2}" 
+        r="${r}" 
+        stroke="currentColor" 
+        stroke-width="${stroke}"
+        stroke-linecap="round"
+        />`
+        return dom + `</svg>`
+
+    }
+    /**
      * Construire le style de notre loader
      * @param {number} w largeur de l'élément 
+     * @param {number} stroke largeur du trait
+     * @param {number} n nombre de section
      * @return {string}
      */
-    buildStyle(w){
+    buildStyle(w, stroke, n){
+        const perimeter = Math.PI * (w - stroke)
         return `<style>
                 :host{
                     display: inline-block;
@@ -44,7 +73,42 @@ class SpinningDots extends HTMLElement{
                 div{
                     width: ${w}px;
                     height: ${w}px;
-                    background: red;
+                    position: relative;
+                }
+                svg{
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                }
+                .circles{
+                    animation: spin 10s linear infinite;
+                }
+                @keyframes spin{
+                    from{transform: rotate(0deg)}
+                    to{transform: rotate(360deg)}
+                }
+                .trail{
+                    stroke-dasharray: ${perimeter};
+                    stroke-dashoffset: ${perimeter + (perimeter / n)};
+                    animation: spintrail 1.6s cubic-bezier(.5, .15, .5, .85) infinite;
+                }
+                .trail circle{
+                    animation: trail 1.6s cubic-bezier(.5, .15, .5, .85) infinite;
+                }
+                @keyframes spintrail{
+                    from{transform: rotate(0deg)}
+                    to{transform: rotate(720deg)}
+                }
+                @keyframes trail{
+                    0%{
+                        stroke-dashoffset: ${perimeter + (perimeter / n)};
+                    }
+                    50%{
+                        stroke-dashoffset: ${perimeter + 3 * (perimeter / n)};
+                    }
+                    100%{
+                        stroke-dashoffset: ${perimeter + (perimeter / n)};
+                    }
                 }
             </style>`
     }
